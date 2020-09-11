b4a2550812ddac88833ed62ff480bb54e62945bc
Ramesh K <ramesh.kannan@smartzi.com>
2020-09-11 09:30:25 +0530

    Initial Commit

---
+++ MapTungiiOSSDK/MapTungiiOSSDK.h
@@ -0,0 +1,19 @@
+//
+//  MapTungiiOSSDK.h
+//  MapTungiiOSSDK
+//
+//  Created by Ramesh K on 11/09/20.
+//  Copyright © 2020 Ramesh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MapTungiiOSSDK.
+FOUNDATION_EXPORT double MapTungiiOSSDKVersionNumber;
+
+//! Project version string for MapTungiiOSSDK.
+FOUNDATION_EXPORT const unsigned char MapTungiiOSSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapTungiiOSSDK/PublicHeader.h>
+
+
